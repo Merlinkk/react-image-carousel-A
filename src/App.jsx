e1cc1bc624ad2b98c66ad1a24e0c2a32fa6f3920
Merlinkk <anshulkashyap2504@gmail.com>
2024-01-15 11:46:42 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import Carousel from "./components/Carousel";
-import "./App.css";
-
+// import "./App.css";
+import ImageData from "./data/CarouselData";
 
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <Carousel />
+        <Carousel ImgData={ImageData} />
       </div>
     );
   }
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,13 +1,76 @@
 import React, { Component } from "react";
 import "./Carousel.css";
-import { images } from "../data/CarouselData";
 // you can explore more - and check as how to use materiul ui
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // implement the class below
 class Carousel extends Component {
-  
+  constructor() {
+    super();
+    this.state = {
+      activeIndex: 0,
+    };
+  }
+
+  nextHandler = () => {
+    if (this.state.activeIndex < this.props.ImgData.length - 1) {
+      this.setState({
+        activeIndex: this.state.activeIndex + 1,
+      });
+    } else {
+      this.setState({
+        activeIndex: 0,
+      });
+    }
+    console.log(this.state.activeIndex);
+  };
+
+  backHandler = () => {
+    if (this.state.activeIndex > 0) {
+      this.setState({
+        activeIndex: this.state.activeIndex - 1,
+      });
+    } else {
+      this.setState({
+        activeIndex: 2,
+      });
+    }
+    console.log(this.state.activeIndex);
+  };
+
+  render() {
+    return (
+      <>
+        <div className="carousel">
+          <div onClick={this.backHandler} className="arrowHolder">
+            <ArrowBackIosIcon className="left-arrow" />
+          </div>
+          <div className="carousel-inner">
+            {this.props.ImgData.map((item, index) => {
+              return (
+                <div
+                  key={index}
+                  className={
+                    index === this.state.activeIndex
+                      ? "carousel-item active"
+                      : "carousel-item"
+                  }
+                >
+                  <h1 className="absoluteText top">{this.props.ImgData[index].title}</h1> 
+                  <h1 className="absoluteText bottom">{this.props.ImgData[index].subtitle}</h1>  
+                  <img src={item.img} alt="carousel-image" />
+                </div>
+              );
+            })}
+          </div>
+          <div onClick={this.nextHandler} className="arrowHolder">
+            <ArrowForwardIosIcon className="right-arrow" />
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default Carousel;
